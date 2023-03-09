3db5e76c2d179c2e7b94d15ddb459109bdba9c80
abc9207144 <sanecrtk@gmail.com>
2023-03-09 22:55:41 +0800

    adding the icon packages module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,19 @@
 module com.siweb.client {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.kordamp.bootstrapfx.core;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
     requires org.json;
     requires java.net.http;
-
     requires de.jensd.fx.glyphs.commons;
     requires de.jensd.fx.glyphs.fontawesome;
+    //img pack of ikonli
+    requires org.kordamp.ikonli.fontawesome5;
+    requires org.kordamp.ikonli.elusive;
+
 
     opens com.siweb to javafx.fxml;
     exports com.siweb;
