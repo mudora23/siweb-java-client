02f801d01c35143a59724e258db7e0fafd2f6606
Benjamin Chau <68836494+swarfte@users.noreply.github.com>
2023-03-18 16:28:01 +0800

    implement modify function for the admin profile , but fail to update the user attributes (such at first name , last name, etc)

--- src/main/java/com/siweb/controller/AdminProfileController.java
+++ src/main/java/com/siweb/controller/AdminProfileController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import org.json.JSONObject;
 
+import java.util.Map;
+
 public class AdminProfileController extends AdminController {
     @FXML
     private TextField fatherNameData;
@@ -25,10 +27,12 @@ public class AdminProfileController extends AdminController {
     @FXML
     private TextField lastNameData;
 
+    private JSONObject originalDate;
     public void initialize() {
         String url = AdminStudentListController.studentURL;
         http.get(url, (JSONObject res) -> {
             Platform.runLater(() -> {
+                originalDate = res;
                 setStudentData(res);
             });
         });
@@ -88,12 +92,35 @@ public class AdminProfileController extends AdminController {
         });
     }
 
-    public void updateStudentData(){
+    @FXML
+    public void updateStudentData() {
         String url = AdminStudentListController.studentURL;
+        // Create a JSONObject to store the updated student data
+        JSONObject updatedData = new JSONObject();
+        JSONObject userData = new JSONObject();
 
+        updatedData.put("fatherName", fatherNameData.getText());
+        updatedData.put("motherName", motherNameData.getText());
+        updatedData.put("address", addressData.getText());
+        updatedData.put("tel", telData.getText());
+        updatedData.put("id", originalDate.getInt("id"));
+
+        userData.put("last_name", lastNameData.getText());
+        userData.put("groups", originalDate.getJSONObject("user").getJSONArray("groups"));
+        userData.put("id", originalDate.getJSONObject("user").getInt("id"));
+        userData.put("first_name", firstNameData.getText());
+        userData.put("url", originalDate.getJSONObject("user").getString("url"));
+        userData.put("email", emailData.getText());
+        userData.put("username", usernameData.getText());
+
+        updatedData.put("user", userData);
+
+        System.out.println(updatedData);
+        // Send the updated data to the server using a PUT request
+        http.put(url,updatedData.toMap(), (JSONObject response) -> {
+            // Handle the server response, e.g., display a success message or handle errors
+            Platform.runLater(this::toAdminProfile);
+        });
     }
-
-
-    
-
 }
+
