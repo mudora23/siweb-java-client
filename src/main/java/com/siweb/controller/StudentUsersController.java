81889b8146fe38ca07b1b95a3f2b36ec951f323e
Benjamin Chau <68836494+swarfte@users.noreply.github.com>
2023-04-09 22:43:54 +0800

    set all user information field to disable

--- src/main/java/com/siweb/controller/StudentUsersController.java
+++ src/main/java/com/siweb/controller/StudentUsersController.java
@@ -45,10 +45,10 @@ public class StudentUsersController extends BaseController{
             Platform.runLater(() ->{
                 leftUserDetailVBox.getChildren().add(new Label("Basic Information"));
                 leftUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("id","ID").setText(String.valueOf(res.getInt("id"))).setDisable(true).build().get());
-                leftUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("username","Username *").setText(res.getString("username")).build().get());
-                leftUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("first_name","First Name").setText(res.getString("first_name")).build().get());
-                leftUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("last_name","Last Name").setText(res.getString("last_name")).build().get());
-                leftUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("email","Email").setText(res.getString("email")).build().get());
+                leftUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("username","Username *").setText(res.getString("username")).setDisable(true).build().get());
+                leftUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("first_name","First Name").setText(res.getString("first_name")).setDisable(true).build().get());
+                leftUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("last_name","Last Name").setText(res.getString("last_name")).setDisable(true).build().get());
+                leftUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("email","Email").setText(res.getString("email")).setDisable(true).build().get());
 
 //                userDetailVBox.getChildren().add(new Separator());
                 JSONObject profile = res.getJSONObject("profile");
@@ -60,11 +60,11 @@ public class StudentUsersController extends BaseController{
 //
 //                rightUserDetailVBox.getChildren().add(new BuilderMFXComboBoxController.Builder("role", "Role *", List.of(new SelectOption("admin"), new SelectOption("lecturer"), new SelectOption("student"))).setValText(newSelection.getProfileRole()).build().get());
 //                rightUserDetailVBox.getChildren().add(new BuilderMFXComboBoxController.Builder("role", "Role *", List.of(new SelectOption("admin"), new SelectOption("lecturer"), new SelectOption("student"))).setValText(newSelection.getProfileRole()).setDisable(true).build().get());
-                rightUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("father_name","Father Name").setText(profile.getString("father_name")).build().get());
-                rightUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("mother_name","Mother Name").setText(profile.getString("mother_name")).build().get());
-                rightUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("address_1","Address 1").setText(profile.getString("address_1")).build().get());
-                rightUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("address_2","Address 2").setText(profile.getString("address_2")).build().get());
-                rightUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("tel","Tel.").setText(profile.getString("tel")).build().get());
+                rightUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("father_name","Father Name").setText(profile.getString("father_name")).setDisable(true).build().get());
+                rightUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("mother_name","Mother Name").setText(profile.getString("mother_name")).setDisable(true).build().get());
+                rightUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("address_1","Address 1").setText(profile.getString("address_1")).setDisable(true).build().get());
+                rightUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("address_2","Address 2").setText(profile.getString("address_2")).setDisable(true).build().get());
+                rightUserDetailVBox.getChildren().add(new BuilderMFXTextFieldController.Builder("tel","Tel.").setText(profile.getString("tel")).setDisable(true).build().get());
             });
         });
 
